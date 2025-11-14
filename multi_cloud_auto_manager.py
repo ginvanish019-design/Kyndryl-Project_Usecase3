175a3ffd9c5c6f041dfb57e86aeb248098ad99a3
ginvanish019-design <gin.vanish019@gmail.com>
2025-11-14 21:45:41 +0530

    Update multi_cloud_auto_manager.py

--- multi_cloud_auto_manager.py
+++ multi_cloud_auto_manager.py
@@ -11,6 +11,8 @@ from azure.mgmt.compute import ComputeManagementClient
 
 # ✔ NEW Azure Monitor client
 from azure.monitor.query import MetricsQueryClient
+from azure.monitor.query import LogsQueryClient
+
 #from azure.monitor.query import MetricAggregationType
 
 
